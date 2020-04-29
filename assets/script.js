925cfbc0f815ef4676b287d553066f2522d8262a
nick <schoessnick@gmail.com>
2020-04-29 14:16:33 -0500

    built basic js functions

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,6 @@
+var eachHR = ["hr1", "hr2", "hr3", "hr4", "hr5", "hr6", "hr7", "hr8", "hr9"];
+var eachBtn = ["#btn1", "#btn2", "#btn3", "#btn4", "#btn5", "#btn6", "#btn7", "#btn8", "#btn9"];
+var i = 0
 //take user input to each box
     //get box by id
 
@@ -14,11 +17,20 @@
     //store function to display user input from local storage for use in multiple circumstances
 
 //Moment.JS
-
+var btn = document.getElementsByClassName("btn")[i];
 var inputs = document.querySelectorAll(".box");
 //for each input do this
-inputs.forEach(function(input){
-    input.addEventListener("click", function(event){
-        event.preventDefault()
-    });
-})
+inputs.forEach(setInput);
+        
+    function setInput() {
+        for (i = 0 ; i < btn.length ; i++) {
+        btn.addEventListener("click", setStorage())}};
+
+    function setStorage() {
+        event.preventDefault();
+        localStorage.setItem("userInput", inputs.value);
+    }
+
+function displayInput() {
+    document.getElementsByClassName(".box") = localStorage.getItem("userInput");
+}
