76d14030ad036cf4c59e67d43a0a378993c01d0e
Nick Schoess <schoessnick@gmail.com>
2020-05-01 13:20:47 -0500

    added live date update at top of the page and linked moment.js file

--- assets/script.js
+++ assets/script.js
@@ -1,42 +1,34 @@
-var eachHR = ["hr1", "hr2", "hr3", "hr4", "hr5", "hr6", "hr7", "hr8", "hr9"];
-var eachBtn = ["#btn1", "#btn2", "#btn3", "#btn4", "#btn5", "#btn6", "#btn7", "#btn8", "#btn9"];
-var i = 0
-//take user input to each box
-    //get box by id
+document.addEventListener('DOMContentLoaded', function() {
+    var eachHR = ["hr1", "hr2", "hr3", "hr4", "hr5", "hr6", "hr7", "hr8", "hr9"];
+    var eachBtn = ["#btn1", "#btn2", "#btn3", "#btn4", "#btn5", "#btn6", "#btn7", "#btn8", "#btn9"];
+    var i = 0
 
-
-//set user input to local storage
-    //capture button click
-    //get value of input
-    //set said value to local storage
-
-
-//display user input
+    var btns = document.querySelectorAll(".btn");
+    var inputs = document.querySelectorAll(".box");
+    //display user input
     //display info from local storage
     //option to clear input by user
     //store function to display user input from local storage for use in multiple circumstances
-var arr = [1,2,3,4,5];
-
-
-
-//Moment.JS
-var btns = document.querySelectorAll(".btn");
-var inputs = document.querySelectorAll(".box");
-//for each input do this
-//inputs.forEach(setInput);
+    //Moment.JS
+    function liveDate() {
+        let dateDisplay = moment().format('LL');
+        document.getElementById("liveDate").append(dateDisplay);
+    }
+    liveDate();
+    //for each input do this
+    //inputs.forEach(setInput);
 
     btns.forEach(function(btn){
         btn.addEventListener("click", function(e) {
             var btnDT = e.target.getAttribute("data-time");
             // Select the input with the attr. data-time="hr1"
-           var selectors = document.querySelectorAll(`[data-time="${btnDT}"]`);
+            var selectors = document.querySelectorAll(`[data-time="${btnDT}"]`);
             var taskInput = selectors[0].value;
 
             setStorage(taskInput, btnDT)
         })
     })
-
-        
+    
     function setInput() {
         for (i = 0 ; i < btns.length ; i++) {
         btns.addEventListener("click", setStorage())}};
@@ -52,9 +44,13 @@ var inputs = document.querySelectorAll(".box");
         //save said array to local storage
     }
 
-function displayInput(i) {
-    var displayTask = JSON.parse(localStorage.getItem("userInput"));
-    i = displayTask;
-    //get previous array from local storage
-    //loop through values to display in cooresponding boxes
+    function displayInput(i) {
+        var displayTask = JSON.parse(localStorage.getItem("userInput"));
+        i = displayTask;
+        //get previous array from local storage
+        for (i = 0; i < displayTask.length; i++) {
+            inputs.appendChild(taskInput);
+        }
+        //loop through values to display in cooresponding boxes
 }
+}, false);
