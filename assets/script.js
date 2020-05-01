5630245852a17807a450e7a792bfd61ef91e6fdd
Nick Schoess <schoessnick@gmail.com>
2020-05-01 11:24:10 -0500

    built logic to set user infor to local storage

--- assets/script.js
+++ assets/script.js
@@ -15,22 +15,46 @@ var i = 0
     //display info from local storage
     //option to clear input by user
     //store function to display user input from local storage for use in multiple circumstances
+var arr = [1,2,3,4,5];
+
+
 
 //Moment.JS
-var btn = document.getElementsByClassName("btn")[i];
+var btns = document.querySelectorAll(".btn");
 var inputs = document.querySelectorAll(".box");
 //for each input do this
-inputs.forEach(setInput);
+//inputs.forEach(setInput);
+
+    btns.forEach(function(btn){
+        btn.addEventListener("click", function(e) {
+            var btnDT = e.target.getAttribute("data-time");
+            // Select the input with the attr. data-time="hr1"
+           var selectors = document.querySelectorAll(`[data-time="${btnDT}"]`);
+            var taskInput = selectors[0].value;
+
+            setStorage(taskInput, btnDT)
+        })
+    })
+
         
     function setInput() {
-        for (i = 0 ; i < btn.length ; i++) {
-        btn.addEventListener("click", setStorage())}};
+        for (i = 0 ; i < btns.length ; i++) {
+        btns.addEventListener("click", setStorage())}};
 
-    function setStorage() {
-        event.preventDefault();
-        localStorage.setItem("userInput", inputs.value);
+    function setStorage(taskInput, btnDT) {
+        console.log(taskInput)
+        var valueToStore = {time: btnDT, value: taskInput}
+        localStorage.setItem("userInput", JSON.stringify(valueToStore));
+        displayInput(taskInput)
+        //get whats currently in local storage
+        //which will be an array
+        //localStorage should be an array of valuetoStore
+        //save said array to local storage
     }
 
-function displayInput() {
-    document.getElementsByClassName(".box") = localStorage.getItem("userInput");
+function displayInput(i) {
+    var displayTask = JSON.parse(localStorage.getItem("userInput"));
+    i = displayTask;
+    //get previous array from local storage
+    //loop through values to display in cooresponding boxes
 }
